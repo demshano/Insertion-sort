b77782420674cd05da2175d0e53cb55eaf04101f
deshan manodya <91672656+demshano@users.noreply.github.com>
2022-02-24 19:39:41 +0530

    Add files via upload

---
+++ Insertion_sort.java
@@ -0,0 +1,23 @@
+public class Insertion_sort{
+	
+	public static void main(String []args){
+		
+		int a[]={1,2,3,10,7};
+		
+		for(int i=1;i<a.length;i++){
+			int temp = a[i];
+			int j=i-1;
+			while(j>=0 && a[j]>temp){
+		a[j+1]=a[j];
+		j--;
+	}
+	
+	a[j+1]=temp;
+}
+
+for(int i=0;i<a.length;i++){
+	System.out.println(a[i]);
+}
+
+}
+}
